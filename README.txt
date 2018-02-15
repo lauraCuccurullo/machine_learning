4cf7d342a3647c2381e1448ac33ab7ac034fc003
lauraCuccurullo <curux@hotmail.it>
2018-02-15 16:22:13 +0100

    generic improvements

--- README.txt
+++ README.txt
@@ -2,6 +2,8 @@ I developed my progect with matlab.
 
 I used 2 datasets, one of fraud credit and the other of skin colour.
 
+The data set of fraud credit is available here : https://drive.google.com/open?id=1HJuILC9uDifutBfZcPAlr4F7qfoL5_BZ
+
 I have implemented ID3 algorithm for building a tree. 
 
 Each folders contains a variation of the ID3 algorithm, with a different number of children.
---
+++ alberoID3Credit4child/PrintTree.m
@@ -0,0 +1,28 @@
+function [] = PrintTree(tree, parent)
+
+% Stampo il nodo corrente
+if (strcmp(tree.value, 'true'))
+    fprintf('parent: %s\ttrue\n', parent);
+    return
+elseif (strcmp(tree.value, 'false'))
+    fprintf('parent: %s\tfalse\n', parent);
+    return
+else
+    % Nodo e attributo utilizzato per lo split
+    fprintf('parent: %s\tattribute: %s\tleftChild:%s\tleftcenterChild:%s\trightcenterChild:%s\trightChild:%s\n', ...
+        parent, tree.value, tree.left.value, tree.leftcenter.value, tree.rightcenter.value, tree.right.value);
+end
+
+% Chiamata ricorsiva di sinistra
+PrintTree(tree.left, tree.value);
+
+% Chiamata ricorsiva di centro
+PrintTree(tree.leftcenter, tree.value);
+
+% Chiamata ricorsiva di centro
+PrintTree(tree.rightcenter, tree.value);
+
+% Chiamata ricorsiva di destra
+PrintTree(tree.right, tree.value);
+
+end
